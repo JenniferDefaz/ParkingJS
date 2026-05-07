98a67e56b53f91e18d6cceb02fdb6ddc7cccf85b
Jennifer Defaz <jenniferdefaz991@gmail.com>
2026-05-07 15:33:33 -0500

    Agrega modelo Tarifa en el admin.py

--- cobro/admin.py
+++ cobro/admin.py
@@ -4,10 +4,12 @@ from django.contrib import admin
 from .models import Cliente
 from .models import Vehiculo
 from .models import Espacio
+from .models import Tarifa
 from .models import Cobro
 
 
 admin.site.register(Cliente)
 admin.site.register(Vehiculo)
 admin.site.register(Espacio)
+admin.site.register(Tarifa)
 admin.site.register(Cobro)
