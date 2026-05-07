1b1855695f724a5f037615ff62cb53eba9755449
Jennifer Defaz <jenniferdefaz991@gmail.com>
2026-05-07 14:52:04 -0500

    Agregamos la clase Espacio en el admin.py

--- cobro/admin.py
+++ cobro/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 
 from .models import Cliente
 from .models import Vehiculo
+from .models import Espacio
 
 admin.site.register(Cliente)
 admin.site.register(Vehiculo)
+admin.site.register(Espacio)
