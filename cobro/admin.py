311fe34016f3d027b80d5d0972255c67dbafbbe8
Jennifer Defaz <jenniferdefaz991@gmail.com>
2026-05-07 14:54:59 -0500

    Agregamos la clase Cobro en el admin.py

--- cobro/admin.py
+++ cobro/admin.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 from .models import Cliente
 from .models import Vehiculo
 from .models import Espacio
+from .models import Cobro
+
 
 admin.site.register(Cliente)
 admin.site.register(Vehiculo)
 admin.site.register(Espacio)
+admin.site.register(Cobro)
